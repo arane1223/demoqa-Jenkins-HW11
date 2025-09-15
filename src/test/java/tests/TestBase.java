3ccd2419f80085b1504c2ca1523faf0aa26ca7fc
sergejgluhov <arane1223@gmail.com>
2025-09-15 23:44:48 +0500

    added config in TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,8 +3,10 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -30,6 +32,7 @@ public class TestBase {
 
     RegistrationForm registrationPage = new RegistrationForm();
     RegistrationResultsComponent registrationResults = new RegistrationResultsComponent();
+    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
     String
             firstName = getFirstName(),
@@ -52,7 +55,9 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        String login = config.login();
+        String password = config.password();
+        Configuration.remote = "https://"+login+":"+password+"@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
