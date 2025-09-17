fe63e8a702c8c01320fb7341114795a1ce7a98a7
sergejgluhov <arane1223@gmail.com>
2025-09-17 10:50:49 +0500

    deleted propertyDriverUrl in Attach

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -45,8 +45,7 @@ public class Attach {
     }
 
     public static URL getVideoUrl() {
-        String propertyDriverUrl = System.getProperty("remoteDriverUrl");
-        String videoUrl = "https://"+propertyDriverUrl+"/video/" + sessionId() + ".mp4";
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
