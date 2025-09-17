4f780a2750ac27be0dfc536f17cdde332e8c34d4
sergejgluhov <arane1223@gmail.com>
2025-09-17 08:07:47 +0500

    added BeforeEach in TestBase and replaced Allure Listener

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -9,6 +9,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationForm;
 import pages.components.RegistrationResultsComponent;
@@ -64,6 +65,10 @@ public class TestBase {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addAllureListener() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
